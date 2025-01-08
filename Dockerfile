4f54a1f9cd1e45dcda13b4963f512b6ce010b508
Dhruv Jain <devopsdhruv@gmail.com>
2025-01-08 15:01:06 +0530

    Flask-app V1

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Use an official Python runtime as a parent image
+FROM python:3.12-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Install required packages
+RUN apt-get update \
+    && apt-get upgrade -y \
+    && apt-get install -y gcc default-libmysqlclient-dev pkg-config \
+    && rm -rf /var/lib/apt/lists/*
+
+# Copy the requirements file into the container
+COPY requirements.txt .
+
+# Install app dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Make port 5000 available to the world outside this container
+EXPOSE 5000
+
+# Run app.py when the container launches
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+# MySQL database configuration using environment variables
+db_config = {
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
+    'database': os.getenv('DB_NAME')
+}
+
+# Function to get a database connection
+def get_db_connection():
+    return mysql.connector.connect(**db_config)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    if request.method == 'POST':
+        if 'submit' in request.form:  # Check if it's the "Submit" button
+            name = request.form['name']
+            email = request.form['email']
+            cursor.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (name, email))
+            conn.commit()
+        elif 'delete' in request.form:  # Check if it's the "Delete" button
+            user_index = int(request.form['index'])
+            cursor.execute("DELETE FROM users WHERE id = %s", (user_index,))
+            conn.commit()
+
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return render_template('user_list.html', users=users)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
