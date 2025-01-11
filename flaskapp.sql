1d8ae01530f4368f39e6de2ed71001d3eba109e5
Dhruv Jain <devopsdhruv@gmail.com>
2025-01-12 01:51:36 +0530

    delete all old files in main branch

--- flaskapp.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS flaskapp;
-
-USE flaskapp;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100)
-);
